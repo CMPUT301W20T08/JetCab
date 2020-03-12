87d3c9043894175433d62450aa4f3b255e60c5d8
Joyce <yunqin@ualberta.ca>
2020-03-12 12:07:06 -0600

    geocoder and map

--- app/src/main/java/com/example/jetcab/MapDisplay.java
+++ app/src/main/java/com/example/jetcab/MapDisplay.java
@@ -1,7 +1,17 @@
 package com.example.jetcab;
 
+import android.content.Context;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.WorkerThread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,8 +21,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MapDisplay extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
+    String start_location;
+    TextView test;
+    Double start_lat, start_lng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,21 +37,74 @@ public class MapDisplay extends AppCompatActivity implements OnMapReadyCallback 
         setContentView(R.layout.map_display);
         MapDisplay.this.setTitle("Map View");
 
+        test = findViewById(R.id.test);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-
-    //not finish
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Canada and move the camera
-        LatLng ca = new LatLng(56.130367, -106.346771);
-        mMap.addMarker(new MarkerOptions().position(ca).title("Marker in Canada"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(ca));
+        // Add a marker of start location
+        Intent intent = getIntent();
+        start_location = intent.getStringExtra("START LOCATION");
+
+        GeocoderHandler geocoderHandler = new GeocoderHandler();
+        getLatLng(start_location, getApplicationContext(), geocoderHandler);
+        //!!!!!get error here, cannot get lat and lng from handler!!!!!
+        if (start_lat == null && start_lng == null) {
+            LatLng from = new LatLng(Double.valueOf(test.getText().toString()),-29.3);
+            mMap.addMarker(new MarkerOptions().position(from).title(start_location));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(from));
+        }
     }
+
+    public static void getLatLng(final String location, final Context context, final Handler handler) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+                Address address;
+                Double lat = null;
+                Double lng = null;
+                try {
+                    List<Address> addresses = geocoder.getFromLocationName(location, 5);
+                    if (addresses != null && addresses.size() > 0) {
+                        address = addresses.get(0);
+                        lat = address.getLatitude();
+                        lng = address.getLongitude();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Message message = Message.obtain();
+                    message.setTarget(handler);
+                    if (lat != null && lng != null) {
+                        Bundle bundle = new Bundle();
+                        bundle.putDouble("LATITUDE", lat);
+                        bundle.putDouble("LONGITUDE", lng);
+                        message.setData(bundle);
+                    }
+                    message.sendToTarget();
+                }
+            }
+        };
+        thread.start();
+    }
+
+    private class GeocoderHandler extends Handler {
+        Double lat, lng;
+        @Override
+        public void handleMessage(Message message) {
+            Bundle bundle = message.getData();
+            lat = bundle.getDouble("LATITUDE");
+            lng = bundle.getDouble("LONGITUDE");
+            test.setText(lat.toString());
+        }
+    }
+
 }
--- app/src/main/java/com/example/jetcab/PostRequest.java
+++ app/src/main/java/com/example/jetcab/PostRequest.java
@@ -1,43 +1,141 @@
 package com.example.jetcab;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class PostRequest extends AppCompatActivity {
+    FusedLocationProviderClient fusedLocationProviderClient;
+    TextInputLayout textInputLayout_from;
+    TextInputEditText editText_from;
+    TextInputLayout textInputLayout_to;
+    TextInputEditText editText_to;
+    Button post_button;
+    Geocoder geocoder;
+    String start_location, end_location;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.post_request);
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
+        //get the current location latitude and longitude
+        //get the current address
+        editText_from = findViewById(R.id.from_editText);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        geocoder = new Geocoder(this, Locale.getDefault());
+        //check whether the location permission is open
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            getLastLocation();
+        } else {
+            Toast.makeText(getApplicationContext(), "Turn on the Location Permission", Toast.LENGTH_LONG).show();
+        }
+
         //click the map icon to from location on map
-        TextInputLayout textInputLayout_from = findViewById(R.id.from_textField);
+        textInputLayout_from = findViewById(R.id.from_textField);
         textInputLayout_from.setEndIconOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent mapIntent = new Intent(v.getContext(), MapDisplay.class);
-                startActivityForResult(mapIntent, 0);
+                start_location = editText_from.getText().toString();
+                if (isAddressValid(start_location)) {
+                    Intent intent = new Intent(v.getContext(), MapDisplay.class);
+                    intent.putExtra("START LOCATION", start_location);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Invalid Start Location Address", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
         //post and save the information of ride, end the activity
-        Button post_button = findViewById(R.id.post_button);
+        post_button = findViewById(R.id.post_button);
         post_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //...
-                finish();
+                // check whether the input address is valid
+                if (isAddressValid(editText_from.getText().toString())) {
+                    Toast.makeText(getApplicationContext(), "valid Start Location Address", Toast.LENGTH_LONG).show();
+                    //...
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Invalid Start Location Address", Toast.LENGTH_LONG).show();
+                }
             }
         });
+    }
+
+    private void getLastLocation() {
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                //get the latitude and longitude of current location
+                Location location = task.getResult();
+                if (location != null) {
+                    String address = null;
+                    Double curr_latitude, curr_longitude;
+                    curr_latitude = location.getLatitude();
+                    curr_longitude = location.getLongitude();
+
+                    //get the address of current location
+                    try {
+                        List<Address> addresses = geocoder.getFromLocation(curr_latitude, curr_longitude, 1);
+                        address = addresses.get(0).getAddressLine(0);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    editText_from.setText(address);
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Turn on the Location Permission", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public boolean isAddressValid(String location) {
+        Geocoder coder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        try {
+            List<Address> addressList = coder.getFromLocationName(location, 1);
+            if (addressList == null && addressList.size() < 1) {
+                return false;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
     }
 }
