ad00005bf2cc9d035b900830a11378f4045d4e86
Marasy8 <zai@ualberta.ca>
2020-03-13 22:06:10 -0600

    cancel prompt now deletes from Requests

--- app/src/main/java/com/example/jetcab/CancelRequestBeforeFragment.java
+++ app/src/main/java/com/example/jetcab/CancelRequestBeforeFragment.java
@@ -10,23 +10,39 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 /**
+ * @author Charles
  * This is the fragment pop up window that
- * prompts the user to cancel the request (Yes or No)
+ * prompts the rider to cancel the request before
+ * it gets accepted (Yes or No)
  */
 
 public class CancelRequestBeforeFragment extends DialogFragment {
+
+    private FirebaseAuth myFirebaseAuth;
+    private FirebaseFirestore myFF;
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
 
+        myFirebaseAuth = FirebaseAuth.getInstance();
+        myFF = FirebaseFirestore.getInstance();
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // pop up window initialization
         return builder
                 .setTitle("CANCEL THE REQUEST")
                 .setMessage("Are you sure you want to cancel the request?")
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
+                        // remove the request from Requests on FireBase
+                        // of the ID of the current user
+                        String userID = myFirebaseAuth.getCurrentUser().getUid();
+                        Request.CancelledRequest(userID, myFF.collection("Requests").document(userID));
                         // finish activity
                         getActivity().finish();
                     }
