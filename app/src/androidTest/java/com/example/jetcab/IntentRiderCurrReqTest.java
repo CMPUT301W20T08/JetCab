980dd86b8d316a0948d5a14b38c507b3170a78a4
Muhammad Mazhar Hussain <hussain2@ualberta.ca>
2020-04-03 16:51:48 -0600

    Update IntentRiderCurrReqTest.java

--- app/src/androidTest/java/com/example/jetcab/IntentRiderCurrReqTest.java
+++ app/src/androidTest/java/com/example/jetcab/IntentRiderCurrReqTest.java
@@ -24,58 +24,6 @@ import static org.junit.Assert.assertTrue;
  * All the UI tests are written here. Robotium test framework is used
  */
 
-//public class IntentRiderCurrReqTest {
-//    private Solo solo;
-//
-//    @Rule
-//    public ActivityTestRule<CurrentRequest> rule = new ActivityTestRule<>(CurrentRequest.class, true, true);
-//
-//    /**
-//     * Runs before all tests and creates solo instance.
-//     * @throws Exception
-//     */
-//    @Before
-//    public void setUp() throws Exception {
-//        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
-//    }
-//
-//    /**
-//     * Gets the Activity
-//     * @throws Exception
-//     */
-//    @Test
-//    public void start() throws Exception {
-//        Activity activity = rule.getActivity();
-//    }
-//
-//    /**
-//     * check whether clicking on the driver image button opens up driver dialog
-//     */
-//    @Test
-//    public void checkDriverInfoTest() {
-//        solo.assertCurrentActivity("Wrong Activity", CurrentRequest.class);
-//        solo.clickOnText("My Current Requests");
-//        ImageButton br = (ImageButton) solo.getView(R.id.driver_profile_image_button);
-//        solo.clickOnView(br);
-//        solo.waitForActivity("CurrReq");
-//        assertTrue("View driver info dialog not working", solo.waitForText("DRIVER:"));
-//        assertTrue("View driver info dialog not working", solo.waitForText("PHONE:"));
-//        assertTrue("View driver info dialog not working", solo.waitForText("EMAIL:"));
-//        solo.assertCurrentActivity("Wrong Activity", CurrentRequest.class);
-//    }
-//
-//    /**
-//     * Closes the activity after each test
-//     * @throws Exception
-//     */
-//    @After
-//    public void tearDown() throws Exception {
-//        solo.finishOpenedActivities();
-//    }
-//}
-//
-////*************************************************************************************************//*************************************************************************************************
-
 public class IntentRiderCurrReqTest {
     private Solo solo;
 
